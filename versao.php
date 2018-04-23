224b64cc97aed3fc39f41e755afd20fcd17220ae
Michel Oliveira <michel.s.oliveira@gmail.com>
2018-04-23 13:56:29 -0300

    teste
    t

---
+++ versao.php
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+<?php
+echo "gabriel nao e viado";
+
+echo "gabriel nao e viado 2";
+
+function gabriel()
+{
+    var teste;
+    return var;
+}
+
+?>
+=======
+>>>>>>> parent of ae96865... gabriel nao e viado
